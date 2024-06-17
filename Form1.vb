80a9c0ec2378ffc997d08a013463abf16a7e1fea
tomas <tomas@LAPTOP-HQ42VN2G>
2024-06-17 19:02:15 -0300

    Agregar archivos de proyecto.

---
+++ Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub cmdEjecutar_Click(sender As Object, e As EventArgs) Handles cmdEjecutar.Click
+        Dim numero1, numero2 As Integer
+        numero1 = txtNumero1.Text
+        numero2 = txtNumero2.Text
+        If numero2 < numero1 Then
+            lblMayorNumero.Text = "El mayor es: " & numero1
+        Else
+            lblMayorNumero.Text = "El mayor es: " & numero2
+        End If
+    End Sub
+End Class
